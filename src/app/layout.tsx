eaf242c77d6b30e95c5093763259a121f3c11454
alihamasdev <alihamasdev@gmail.com>
2025-07-02 04:05:29 +0500

    add SVG assets and implement theme provider with header and footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/lib/theme-provider";
 
 import "./globals.css";
+import { Footer } from "@/components/footer";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -17,10 +19,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body style={geistSans.style}>
-				{children}
-				<Toaster />
+				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+					<main className="bg-muted/60 flex min-h-dvh w-full flex-col">
+						<header className="w-full px-4">
+							<div className="container mx-auto flex items-center justify-between py-4">
+								<h1 className="text-3xl font-bold">Cricket Scorecard Generator</h1>
+							</div>
+						</header>
+						<div className="container mx-auto">{children}</div>
+						<Footer />
+					</main>
+					<Toaster />
+				</ThemeProvider>
 			</body>
 		</html>
 	);
---
+++ src/components/footer.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { MoonStar, SunMedium } from "lucide-react";
+import { useTheme } from "next-themes";
+
+export function Footer() {
+	const { setTheme } = useTheme();
+
+	return (
+		<footer className="mt-auto w-full border-t px-4">
+			<div className="container mx-auto flex items-center justify-between py-4">
+				<p className="text-muted-foreground text-sm">
+					&#169; 2025{" "}
+					<a href="http://alihamas.vercel.app" target="_blank" className="hover:underline">
+						Ali Hamas
+					</a>
+					, Inc.
+				</p>
+				<div className="flex items-center rounded-full border p-0.5">
+					<div onClick={() => setTheme("light")} className="bg-muted cursor-pointer rounded-full p-1 transition-colors dark:bg-transparent">
+						<SunMedium className="dark:text-muted-foreground text-foreground size-3.5" />
+					</div>
+					<div onClick={() => setTheme("dark")} className="dark:bg-muted cursor-pointer rounded-full bg-transparent p-1 transition-colors">
+						<MoonStar className="text-muted-foreground dark:text-foreground size-3.5" />
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+}
